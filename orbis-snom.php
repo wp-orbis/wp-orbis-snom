c851f0659f0a4775363bcf6a91255b84b7355848
Remco Tolsma <info@remcotolsma.nl>
2012-08-01 19:09:37 +0200

    Initial release

---
+++ orbis-snom.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: Orbis SNOM
+Plugin URI: http://pronamic.eu/wp-plugins/orbis-snom/
+Description: Extends the Orbis plugin extends the Orbis plugin with some SNOM phone functions.
+
+Version: 0.1
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://pronamic.eu/
+License: GPL
+*/
+
+function orbis_snom_user_profile( $user ) {
+	?>
+	<h3><?php _e( 'SNOM', 'orbis_snom' ); ?></h3>
+
+	<table class="form-table">
+		<tr>
+			<th>
+				<label for="orbis_snom_web_user_interface_url">
+					<?php _e( 'Web User Interface URL', 'orbis_snom' ); ?>
+				</label>
+			</th>
+			<td>
+				<input id="orbis_snom_web_user_interface_url" type="text" name="_orbis_snom_web_user_interface_url" value="<?php echo esc_attr( get_user_meta( $user->ID, '_orbis_snom_web_user_interface_url', true ) ); ?>" />
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+
+add_action( 'show_user_profile', 'orbis_snom_user_profile' );
+add_action( 'edit_user_profile', 'orbis_snom_user_profile' );
+
+function orbis_snom_user_update( $user_id ) {
+	$snom_url = filter_input( INPUT_POST, '_orbis_snom_web_user_interface_url', FILTER_SANITIZE_STRING );
+
+	if( empty ( $snom_url ) ) {
+		delete_user_meta( $user_id , '_orbis_snom_web_user_interface_url' );
+	} else {
+		update_user_meta( $user_id, '_orbis_snom_web_user_interface_url', $snom_url );
+	}
+}
+
+add_action( 'personal_options_update', 'orbis_snom_user_update' );
+add_action( 'edit_user_profile_update', 'orbis_snom_user_update' );
+
+function orbis_snom_call_form( $number = '' ) {
+	$current_user = wp_get_current_user();
+
+	$url = get_user_meta( $current_user->ID, '_orbis_snom_web_user_interface_url', true );
+
+	?>
+	<form method="post" action="<?php echo esc_attr( $url ); ?>" target="_blank">
+		<div>
+			<input name="NUMBER" type="text" value="<?php echo esc_attr( $number ); ?>" />
+			<input name="DIAL" type="submit" value="<?php esc_attr_e( 'Call', 'orbis_snom' ); ?>" />
+		</div>
+	</form>
+	<?php		
+}
+
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Shopp (nl) ===
+Contributors: pronamic, remcotolsma
+Tags: orbis, snom, phone
+Donate link: http://pronamic.eu/donate/?for=wp-plugin-orbis-snom&source=wp-plugin-readme-txt
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 0.1
+
+This WordPress plugin extends the Orbis plugin with some SNOM phone functions.
+
+== Description ==
+
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your 
+WordPress installation and then activate the Plugin from Plugins page.
+
+
+== Changelog ==
+
+= 0.1 =
+*	Initial release
+
+
+== Links ==
+
+*	[Pronamic](http://pronamic.eu/)
+*	[Remco Tolsma](http://remcotolsma.nl/)
+*	[Markdown's Syntax Documentation][markdown syntax]
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
+
+
+== Pronamic plugins ==
+
+*	[Pronamic Google Maps](http://wordpress.org/extend/plugins/pronamic-google-maps/)
+*	[Gravity Forms (nl)](http://wordpress.org/extend/plugins/gravityforms-nl/)
+*	[Pronamic Page Widget](http://wordpress.org/extend/plugins/pronamic-page-widget/)
+*	[Pronamic Page Teasers](http://wordpress.org/extend/plugins/pronamic-page-teasers/)
+*	[Maildit](http://wordpress.org/extend/plugins/maildit/)
+*	[Pronamic Framework](http://wordpress.org/extend/plugins/pronamic-framework/)
+*	[Pronamic iDEAL](http://wordpress.org/extend/plugins/pronamic-ideal/)
+
